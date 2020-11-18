098a56a4ed5aac1ead5a2cc6d06724fc9d54245d
cheko0 <cheko.rosazz@gmail.com>
2020-11-17 20:23:13 -0600

    Add histories

--- src/app/features/dashboard/components/histoy-list/history-list.component.ts
+++ src/app/features/dashboard/components/histoy-list/history-list.component.ts
@@ -1,26 +1,96 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: "history-list",
   templateUrl: "./history-list.component.html",
-  styleUrls: ["./history-list.component.scss"],
+  styleUrls: ["./history-list.component.scss"]
 })
 export class HistoryListComponent implements OnInit {
-  dataSource: any;
-  _meat: any;
+  public _meat:any=null;
 
-  displayedColumns: string[] = ["Number", ""];
+  displayedColumnsOutputs:string[]=['number','name','date','quantity']
+  displayedColumnInputs:string[]=['number','receive','weigth','date']
+  displayedColumnFormulations:string[]=['number','lotDay','product','temp','elaborated']
+  displayedColumnProcess:string[]=['number','lotDay','product','lastProcess','dateStart','dateEnd']
+  displayedColumnOven:string[]=['number','lotDay','product','hour','oven','revisions'];
+  displayedColumnPacking:string[]=['number','date','product','lotDay','presentation','quantity','weight']
+  outputs:any[]=[];
+  inputs:any[]=[];
+  formulations:any[]=[];
+  process:any[]=[];
+  ovens:any[]=[];
+  packing:any[]=[];
+  datasourceOutputs:MatTableDataSource<any>;
+  datasourceInputs:MatTableDataSource<any>;
+  datasourceFormulations:MatTableDataSource<any>;
+  datasourceProcess:MatTableDataSource<any>;
+  datasourceOven:MatTableDataSource<any>;
+  datasourcePacking:MatTableDataSource<any>;
+  @Input() public set meat(val: any){
+    if(val){
+    this._meat=val;
+    console.log(this._meat);
+    if(this._meat.outputs && this._meat.outputs.length){
+      this.outputs=this._meat.outputs;
+      this.resetTable();
+    }
+    if(this._meat.entranceMeat && this._meat.entranceMeat.length){
+      this.inputs=this._meat.entranceMeat;
+      this.resetTable();
+    }
 
-  @Input() public set meat(val: any) {
-    this._meat = val;
-    this.receiver();
+    if(this._meat.formulation && this._meat.formulation.length){
+      this.formulations=this._meat.formulation;
+      this.resetTable();
+    }
+    if(this._meat.process && this._meat.process.length){
+      this.process=this._meat.process;
+      this.resetTable();
+    }
+
+    if(this._meat.oven && this._meat.oven.length){
+      this.ovens=this._meat.oven;
+      this.resetTable();
+    }
+
+    if(this._meat.packingDate && this._meat.packingDate.length){
+      this.packing=[];
+      this._meat.packingDate.map((x)=>{
+        for(let output of x.properties){
+          this.packing.push({
+            date: x.date,
+            product:output.product,
+            lotDay:x.newLot,
+            presentation: output.presentation,
+            quantity: output.quantity,
+            weight: output.weight
+          })
+        }
+      });
+      this.resetTable();
+    }
+  }
+    
   }
 
-  constructor() {}
+  constructor() {
+    this.datasourceOutputs=new MatTableDataSource();
+    this.datasourceInputs= new MatTableDataSource();
+    this.datasourceFormulations= new MatTableDataSource();
+    this.datasourceProcess= new MatTableDataSource();
+    this.datasourceOven = new MatTableDataSource();
+    this.datasourcePacking= new MatTableDataSource();
+  }
 
   ngOnInit() {}
 
-  receiver() {
-    console.log(this._meat);
+  resetTable(){
+    this.datasourceOutputs.data=this.outputs;
+    this.datasourceInputs.data=this.inputs;
+    this.datasourceFormulations.data=this.formulations;
+    this.datasourceProcess.data=this.process;
+    this.datasourceOven.data=this.ovens;
+    this.datasourcePacking.data=this.packing;
   }
 }
